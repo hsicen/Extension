4243420a3099a9720b7fbe9dcb25f36a185e47f3
hsicen <codinghuang@163.com>
2021-03-21 13:17:25 +0800

    自定义ViewGroup抽象类

--- app/src/main/java/com/hsicen/extension/MainActivity.kt
+++ app/src/main/java/com/hsicen/extension/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.hsicen.extension
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,8 +9,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnBread.setOnClickListener {
-            startActivity(Intent(this, BreadActivity::class.java))
-        }
     }
 }
